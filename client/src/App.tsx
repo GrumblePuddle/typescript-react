6a5bcfa2fd68b02509da50ef68e104ce85d61c48
Gustav Liffner <gurra_liffner@hotmail.com>
2025-01-28 21:44:53 +0000

    Initial commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,100 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [result, setResult] = useState<string>("");
+
+  const handleClick = (e) => setResult(result.concat(e.target.id));
+
+  const clear = () => setResult("");
+
+  const deleteEL = () => setResult(result.slice(0, -1));
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  };
 
   return (
     <>
-      <div>Hello</div>
+      <div className=" bg-blue-700 w-80 mx-auto mt-10 p-4 rounded-lg shadow-lg">
+        <input
+          className="w-full text-right py-4 px-5 text-2x1 bg-transparent text-white"
+          type="text"
+          value={result}
+          disabled
+        />
+        <div className="buttons">
+          <button className="operator" onClick={clear}>
+            AC
+          </button>
+          <button className="operator" onClick={deleteEL}>
+            DE
+          </button>
+
+          <button id="." className="operator" onClick={handleClick}>
+            .
+          </button>
+          <button id="/" className="operator" onClick={handleClick}>
+            /
+          </button>
+
+          <button id="7" className="number" onClick={handleClick}>
+            7
+          </button>
+          <button id="8" className="number" onClick={handleClick}>
+            8
+          </button>
+          <button id="9" className="number" onClick={handleClick}>
+            9
+          </button>
+
+          <button id="*" className="operator" onClick={handleClick}>
+            *
+          </button>
+
+          <button id="4" className="number" onClick={handleClick}>
+            4
+          </button>
+          <button id="5" className="number" onClick={handleClick}>
+            5
+          </button>
+          <button id="6" className="number" onClick={handleClick}>
+            6
+          </button>
+
+          <button id="-" className="operator" onClick={handleClick}>
+            -
+          </button>
+
+          <button id="1" className="number" onClick={handleClick}>
+            1
+          </button>
+          <button id="2" className="number" onClick={handleClick}>
+            2
+          </button>
+          <button id="3" className="number" onClick={handleClick}>
+            3
+          </button>
+
+          <button id="+" className="operator" onClick={handleClick}>
+            +
+          </button>
+
+          <button id="00" className="number" onClick={handleClick}>
+            00
+          </button>
+          <button id="0" className="number" onClick={handleClick}>
+            0
+          </button>
+
+          <button id="=" className="operator col-span-2" onClick={calculate}>
+            =
+          </button>
+        </div>
+      </div>
     </>
   );
 }
